49bdddb250002835d5f5bf2179f6c01fe241e1fa
Ricardo Cardoso <ricardo.cardoso@certponto.com.br>
2017-06-14 14:29:00 -0300

    implementing index.android.js

--- index.android.js
+++ index.android.js
@@ -1,1 +1,12 @@
+var React = require('react');
+var Text = require('react-native').Text;
+var AppRegistry = require('react-native').AppRegistry;
 
+const App = function () {
+  
+  return (
+      <Text> Meu primeiro APP.</Text>
+    );
+};
+
+AppRegistry.registerComponent('app_code_generator', function() {return App});
