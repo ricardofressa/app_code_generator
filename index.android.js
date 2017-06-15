8da06c518d80f70aaee3f98a2ba850bd32b834c6
Ricardo Cardoso <ricardo.cardoso@certponto.com.br>
2017-06-15 00:18:34 -0300

    Implemented random number

--- index.android.js
+++ index.android.js
@@ -1,11 +1,25 @@
 var React = require('react');
 var Text = require('react-native').Text;
+var View = require('react-native').View;
+var Button = require('react-native').Button;
 var AppRegistry = require('react-native').AppRegistry;
 
+const generateRandomNumbers = () => {
+  var random_number = Math.random();
+  random_number = Math.floor(random_number * 10);
+  alert(random_number);
+}
+
 const App = () => {
-  
   return (
-      <Text> Meu primeiro APP.</Text>
+      <View>
+        <Text> Gerador de números randômicos </Text>
+        <Button
+          title="Gerar Número"
+          onPress={generateRandomNumbers} 
+        />
+      </View>
+
     );
 };
 
